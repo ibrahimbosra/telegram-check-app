3996642062d0bc93f73bbf0605ee63037a86b891
ibrahimbosra <109563092+ibrahimbosra@users.noreply.github.com>
2025-06-28 00:37:40 +0300

    Update check-membership.js

--- api/check-membership.js
+++ api/check-membership.js
@@ -1,1 +1,20 @@
+export default async function handler(req, res) {
+  const BOT_TOKEN = '7759244119:AAGi2FCrKpRxAL-LmXBoT9YARKTkf1H3P6I';
+  const CHANNEL_USERNAME = '@MegaMiningCrypto';
+  const { user_id } = req.query;
 
+  const url = `https://api.telegram.org/bot${BOT_TOKEN}/getChatMember?chat_id=${CHANNEL_USERNAME}&user_id=${user_id}`;
+
+  try {
+    const tgRes = await fetch(url);
+    const json = await tgRes.json();
+
+    if (json.ok) {
+      return res.status(200).json({ ok: true, status: json.result.status });
+    } else {
+      return res.status(200).json({ ok: false });
+    }
+  } catch (err) {
+    return res.status(500).json({ ok: false, error: err.message });
+  }
+}
